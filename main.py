2ac5777462b01a743eff4397d31ea45d6d36f42a
Hipp <arthur.ramolet@hotmail.fr>
2021-03-25 00:38:19 +0100

    Correction on image processing to avoid missing glyphs and coordinates offset

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from imutils import contours
 import os
 from natsort import natsorted, ns
 import shutil
+import numpy as np
 
 N_template = cv2.imread('Data/Patterns/N.png')
 S_template = cv2.imread('Data/Patterns/S.png')
@@ -33,10 +34,10 @@ def parse_image(image_path):
 
     print(image.shape[1])
 
-    scale_factor = 3
-    area_size_threshold = 500
+    scale_factor = 4
+    area_size_threshold = 400
 
-    image = cv2.resize(image, (image.shape[1] * scale_factor, image.shape[0] * scale_factor))
+    image = cv2.resize(image, (image.shape[1] * (scale_factor), image.shape[0] * (scale_factor+2)))
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)[1]
     laplacian = cv2.Laplacian(thresh, cv2.CV_8UC1, ksize=3)
@@ -54,6 +55,7 @@ def parse_image(image_path):
     eyes = []
 
     for c in cnts:
+
         area = cv2.contourArea(c)
         if area > area_size_threshold:
             x, y, w, h = cv2.boundingRect(c)
@@ -62,8 +64,11 @@ def parse_image(image_path):
             eyes.append(eye)
             res = match_template(eye)
             os.makedirs(out_folder, exist_ok=True)
+
+
             tmp_file_name = str(y) + "_" + str(x) + "_" + match_template(eye) + '.png'
             cv2.imwrite(os.path.join(out_folder, tmp_file_name), eye)
+
 
     cv2.imwrite(image_name_without_ext + ".png", image)
     # Dirty name parsing because I'm still lazy
@@ -71,15 +76,32 @@ def parse_image(image_path):
     cpt_x = 0
     cpt_y = 0
     max_x = 0
+    last_y = 0
+
+    #correct y offcet
     for filename in inList:
         filename_without_ext = os.path.splitext(filename)[0]
         filename_tokens = filename_without_ext.split('_')
         x = int(filename_tokens[1])
         y = int(filename_tokens[0])
         orientation = filename_tokens[2]
-        if x < max_x:
+        if (y < last_y + 10) and last_y != 0:
+            y = last_y
+        file_out_name = str(y) + "_" + str(x) + "_" + orientation + ".png"
+        os.rename(os.path.join(out_folder, filename), os.path.join(out_folder, file_out_name))
+        last_y = y
+
+    inList = natsorted(os.listdir(out_folder), alg=ns.PATH)
+    for filename in inList:
+        filename_without_ext = os.path.splitext(filename)[0]
+        filename_tokens = filename_without_ext.split('_')
+        x = int(filename_tokens[1])
+        y = int(filename_tokens[0])
+        orientation = filename_tokens[2]
+        if x < (max_x):
             cpt_x = 0
             cpt_y += 1
+
         max_x = x
         file_out_name = str(cpt_y) + "_" + str(cpt_x) + "_" + orientation + ".png"
         os.rename(os.path.join(out_folder, filename), os.path.join(out_folder, file_out_name))
@@ -88,16 +110,53 @@ def parse_image(image_path):
         pattern_out_folder = os.path.join("Out", "PatternChecker", orientation)
         os.makedirs(pattern_out_folder, exist_ok=True)
         shutil.copyfile(os.path.join(out_folder, file_out_name),
-                        os.path.join(pattern_out_folder, image_name_without_ext + "_" + file_out_name))
+        os.path.join(pattern_out_folder, image_name_without_ext + "_" + file_out_name))
 
 
-parse_image('Data/E1.png')
-parse_image('Data/E2.png')
-parse_image('Data/E3.png')
-parse_image('Data/E4.png')
-parse_image('Data/E5.png')
+def parse_all_data():
+    parse_image('Data/E1.png')
+    parse_image('Data/E2.png')
+    parse_image('Data/E3.png')
+    parse_image('Data/E4.png')
+    parse_image('Data/E5.png')
 
-parse_image('Data/W1.png')
-parse_image('Data/W2.png')
-parse_image('Data/W3.png')
-parse_image('Data/W4.png')
+    parse_image('Data/W1.png')
+    parse_image('Data/W2.png')
+    parse_image('Data/W3.png')
+    parse_image('Data/W4.png')
+
+def load_as_array(path):
+    inList = natsorted(os.listdir(path), alg=ns.PATH)
+    message = {}
+    for filename in inList:
+
+        filename_without_ext = os.path.splitext(filename)[0]
+        filename_tokens = filename_without_ext.split('_')
+        x = int(filename_tokens[1])
+        y = int(filename_tokens[0])
+        orientation = filename_tokens[2]
+        if y not in message:
+            message[y] = {}
+        message[y][x] = orientation
+
+    return message
+
+def parse_to_trigram(message):
+    pass
+
+def pretty_print_message(message):
+    for coord_x in message:
+        str = ""
+        for coord_y in message[coord_x]:
+            str += message[coord_x][coord_y]+" "
+
+        print(str)
+
+folders = ['Out/E1','Out/E2','Out/E3','Out/E4','Out/E5','Out/W1','Out/W2','Out/W3','Out/W4']
+
+parse_all_data()
+for folder in folders:
+    message = load_as_array(folder)
+    pretty_print_message(message)
+    print()
+
